e06966195fc257d43b39c361c7c48078b96d366b
prajwal-0706 <prajwalgadhave356@gmail.com>
2024-02-10 11:45:16 +0530

    feat: Backend initialised

---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: 'https://clear-cheetah-73.clerk.accounts.dev',
+      applicationID: 'convex',
+    },
+  ],
+};
---
+++ convex/documents.ts
@@ -0,0 +1,3 @@
+import { v } from 'convex/values';
+import { mutation, query } from './_generated/server';
+import { Doc, Id } from './_generated/dataModel';
