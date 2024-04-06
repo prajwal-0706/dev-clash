5f96fd16c9ae67b0203bde46becbbf25d1a03e9f
DujalSawarkar <dujalsawar5@gmail.com>
2024-04-06 16:41:52 +0530

    feat : stats component completed

--- app/(landing page)/_components/Stats.tsx
+++ app/(landing page)/_components/Stats.tsx
@@ -3,37 +3,46 @@ import Image from "next/image";
 import { CircleDollarSign, Fence, Tag } from "lucide-react";
 const Stats = () => {
   return (
-    <div className="flex w-11/12 mx-auto bg-gray-100 ">
-      <div className="capitalize">
-        <h2 className="">the new way to find your new home</h2>
+    <div className="flex w-10/12 mx-auto bg-gray-100 ">
+      <div className=" w-3/5 capitalize p-20   ">
+        <h2 className="text-blue-900 text-5xl font-bold">the new way to find your new home</h2>
 
-        <p>
-          Find your dream place to live in with more than 10k+ property listed
+        <p className="pr-28 pt-8 pb-8 pl-2 font-semibold text-gray-700">
+          Find your dream place to live in with more than 10k+ property listed.
         </p>
 
-        <div className="flex">
-          <div className="p-1 rounded-full border border-black">
-            <div className="bg-gray-200 p-2 rounded-full">
-              <Tag />
+        <div className="flex justify-between items-center w-3/4 ml-8">
+          <div className="flex flex-col justify-center items-center gap-4">
+            <div className="p-1 rounded-full border border-gray-400">
+              <div className="bg-gray-200 p-4 rounded-full ">
+                <Tag className="h-[30px] w-[30px]  "/>
+              </div>
             </div>
+            <div className="font-semibold text-2xl ">7.1%</div>
           </div>
-          <div className="p-1 rounded-full border border-black">
-            <div className="bg-gray-200 p-2 rounded-full">
-              <Fence />
+          <div className="flex flex-col justify-center items-center gap-4">
+            <div className="p-1 rounded-full border border-gray-400">
+              <div className="bg-gray-200 p-4 rounded-full">
+                <Fence className="h-[30px] w-[30px]  "/>
+              </div>
             </div>
+            <div className="font-semibold text-2xl ">2000</div>
           </div>
-          <div className="p-1 rounded-full border border-black">
-            <div className="bg-gray-200 p-2 rounded-full">
-              <CircleDollarSign />
+          <div className="flex flex-col justify-center items-center gap-4">
+            <div className="p-1 rounded-full border border-gray-400">
+              <div className="bg-gray-200 p-4 rounded-full">
+                <CircleDollarSign className="h-[30px] w-[30px]  "/>
+              </div>
             </div>
+            <div className="font-semibold text-2xl ">1300</div>
           </div>
         </div>
       </div>
-      <div className="flex ">
+      <div className="">
         <Image
           className="rounded-md m-4 float-right"
           src="/images/home-about.png"
-          width={300}
+          width={400}
           height={100}
           alt="card"
         />
